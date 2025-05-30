ccfa8684c71edf0fb57378e31dba0bd07fc3f8ab
yuito-kijima <pmppmppmppmppmp@yahoo.co.jp>
2025-05-30 13:19:29 +0900

    step07: 複数の円を波のように揺らすアニメーション

--- sketch.js
+++ sketch.js
@@ -1,15 +1,23 @@
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(600, 400);
 }
 
 function draw() {
     background(255);
 
-    let r = 128 + sin(frameCount * 0.05) * 127;
-    let g = 128 + sin(frameCount * 0.03 + PI / 2) * 127;
-    let b = 128 + sin(frameCount * 0.04 + PI) * 127;
+    let count = 20;
+
+    for (let i = 0; i < count; i++) {
+        let x = map(i, 0, count -1, 50, width - 50);
+        let y = height / 2 + sin(frameCount * 0.1 + i * 0.4) * 50;
+
+        let r = 128 + sin(frameCount * 0.05 + i * 0.2) * 127;
+        let g = 128 + sin(frameCount * 0.07 + i * 0.2 + PI / 3) * 127;
+        let b = 128 + sin(frameCount * 0.09 + i * 0.2 + PI * 2 / 3) * 127;
+    }
+
 
     fill(r, g, b);
     noStroke();
-    ellipse(width / 2, height / 2, 100, 100);  
+    ellipse(x, y, 30, 30);  
 }
