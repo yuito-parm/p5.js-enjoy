d81bd2559f28986e9506110025fb609b2e4470ea
yuito-kijima <pmppmppmppmppmp@yahoo.co.jp>
2025-05-30 11:53:51 +0900

    step04: クリックでランダムな円を追加

--- sketch.js
+++ sketch.js
@@ -1,26 +1,28 @@
-let x, y;
-let r = 100;
-let g = 150;
-let b = 255;
+circles = [];
 
 
 function setup() {
     createCanvas(400, 400);
-    x = width / 2;
-    y = height / 2;
+    background(255);
 }
 
 function draw() {
     background(255);
-    x = lerp(x, mouseX, 0.1);
-    y = lerp(y, mouseY, 0.1);
-    fill(r, g, b,);
-    noStroke();
-    ellipse(x, y, 50, 50);
+
+    for (let c of circles) {
+        fill(c.r, c.g, c.b,);
+        noStroke();
+        ellipse(c.x, c.y, 50, 50);
+    }
 }
 
 function mousePressed() {
-    r = random(0, 256);
-    g = random(0, 256);
-    b = random(0, 256);
+    let newCircle = {
+        x: mouseX,
+        y: mouseY,
+        r: random(0, 256),
+        g: random(0, 256),
+        b: random(0, 256)
+    }
+    circles.push(newCircle);
 }
