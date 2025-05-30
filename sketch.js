ac5cfae4d69a68626ba9ea0f1a1286abe0a44247
yuito-kijima <pmppmppmppmppmp@yahoo.co.jp>
2025-05-30 12:57:47 +0900

    step05: frameCountとsinでぷるぷるアニメーション

--- sketch.js
+++ sketch.js
@@ -1,28 +1,13 @@
-circles = [];
-
-
 function setup() {
     createCanvas(400, 400);
-    background(255);
 }
 
 function draw() {
     background(255);
 
-    for (let c of circles) {
-        fill(c.r, c.g, c.b,);
-        noStroke();
-        ellipse(c.x, c.y, 50, 50);
-    }
-}
+    let size = 50 + sin(frameCount * 0.1) * 20;
 
-function mousePressed() {
-    let newCircle = {
-        x: mouseX,
-        y: mouseY,
-        r: random(0, 256),
-        g: random(0, 256),
-        b: random(0, 256)
-    }
-    circles.push(newCircle);
+    fill(150, 200, 255);
+    noStroke();
+    ellipse(width / 2, height / 2, size, size);  
 }
