ecb168a1c679cc4174e4c7c3c274caa1b1308720
yuito-kijima <pmppmppmppmppmp@yahoo.co.jp>
2025-05-30 11:42:18 +0900

    step03: マウスをふわっと追いかける動きに変更

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+let x, y;
 let r = 100;
 let g = 150;
 let b = 255;
@@ -5,10 +6,14 @@ let b = 255;
 
 function setup() {
     createCanvas(400, 400);
+    x = width / 2;
+    y = height / 2;
 }
 
 function draw() {
     background(255);
+    x = lerp(x, mouseX, 0.1);
+    y = lerp(y, mouseY, 0.1);
     fill(r, g, b,);
     noStroke();
     ellipse(mouseX, mouseY, 50, 50);
